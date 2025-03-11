0b65d6030af2b58e966645fa9b7f86fcac4bd25a
lianasabino <lianasabino@hotmail.com>
2025-03-10 22:04:55 -0300

    Creating homeComponent

---
+++ challenge-angular-app/src/app/pages/home/home.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [MatCardModule, RouterLink, MatButtonModule],
+  template: `
+    <div class="home-container">
+      <mat-card class="home-card" appearance="outlined">
+        <mat-card-header>
+          <mat-card-title>Bem-vindo ao sistema de cadastro</mat-card-title>
+          <mat-card-subtitle>Desafio em angular 19.</mat-card-subtitle>
+        </mat-card-header>
+
+        <mat-card-content>
+          <h2>Funcionalidades</h2>
+
+          <div class="features">
+            <div class="feature-container">
+              <div class="feature-text">
+                <h3>Cadastro de clientes</h3>
+                <p>CRUD de clientes com integração API via cep</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="features">
+            <div class="feature-container">
+              <div class="feature-text">
+                <h3>Cadastro de produtos</h3>
+                <p>CRUD de produtos</p>
+              </div>
+            </div>
+          </div>
+        </mat-card-content>
+
+        <mat-card-actions>
+          <button color="primary" routerLink="/clientes" mat-raised-button>
+            Ir para clientes
+          </button>
+
+          <button color="accent" routerLink="/produtos" mat-raised-button>
+            Ir para produtos
+          </button>
+        </mat-card-actions>
+      </mat-card>
+    </div>
+  `,
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
